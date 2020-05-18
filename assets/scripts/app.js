5e86fe849042d66df3be4a6a0d74d655427010f0
Mark Wilde <markwilde29@gmail.com>
2020-05-18 14:35:43 +0100

    Refactored code. Created a new function to handle all attack logic and game outcomes, called this function in the current attack functions and passed it an arguement so that each function works as before.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,8 +7,14 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler() {
-  const damage = dealMonsterDamage(ATTACK_VALUE);
+function attackMonster(mode) {
+  let maxDamage;
+  if (mode === 'ATTACK') {
+    maxDamage = ATTACK_VALUE;
+  } else if (mode === 'STRONG_ATTACK') {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+  const damage = dealMonsterDamage(maxDamage);
   currentMonsterHealth -= damage;
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
@@ -17,22 +23,15 @@ function attackHandler() {
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
     alert('You have sustained a lethal injury, your time is finished here..');
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
-    alert("it's a draw, you're both dead!! hahahahahaha");
+    alert("It's a draw, you're both dead!! hahahahahaha");
   }
+}
+function attackHandler() {
+  attackMonster('ATTACK');
 }
 
 function strongAttackHandler() {
-  const damage = dealMonsterDamage(STRONG_ATTACK_VALUE);
-  currentMonsterHealth -= damage;
-  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
-  currentPlayerHealth -= playerDamage;
-  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
-    alert('You won!');
-  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
-    alert('You have sustained a lethal injury, your time is finished here..');
-  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
-    alert("it's a draw, you're both dead!! hahahahahaha");
-  }
+  attackMonster('STRONG_ATTACK');
 }
 
 attackBtn.addEventListener('click', attackHandler);
